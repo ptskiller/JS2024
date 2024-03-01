38d4e9b643923a09a8cc7b37e426096ed03d25af
ptskiller <mardzia75@gmail.com>
2024-03-01 04:05:02 +0100

    1

---
+++ skripty.js
@@ -0,0 +1,22 @@
+function z35() {
+    window.open("z35_md.html", "okienko_z28","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+ 
+ function z36() {
+    window.open("z36_md.html", "okienko_z29","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+ 
+ function z38() {
+    window.open("z37_md.html", "okienko_z30","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ 
+ function z38_w() {
+    window.open("z38_md.html", "okienko_z31","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ 
+ function z39() {
+    window.open("z39_md.html", "okienko_z32","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ function z40() {
+    window.open("z40_md.html", "okienko_z30_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+ }
